6cda9f82667830543e51571f6ce1c4f0f4d46e2e
Maria <mengozzi.maria@gmail.com>
2024-02-16 09:42:35 +0100

    feat: update sendwhatmsg_instantly with double enter

--- pywhatkit/whats.py
+++ pywhatkit/whats.py
@@ -25,7 +25,6 @@ def sendwhatmsg_instantly(
         close_time: int = 3,
 ) -> None:
     """Send WhatsApp Message Instantly"""
-
     if not core.check_number(number=phone_no):
         raise exceptions.CountryCodeException("Country Code Missing in Phone Number!")
 
@@ -34,7 +33,17 @@ def sendwhatmsg_instantly(
         raise exceptions.InvalidPhoneNumber("Invalid Phone Number.")
 
     web.open(f"https://web.whatsapp.com/send?phone={phone_no}")
-    time.sleep(wait_time)
+    
+    web.open(f"https://web.whatsapp.com/send?phone={phone_no}&text={quote(message)}")
+    time.sleep(4)
+    pg.click(core.WIDTH / 2, core.HEIGHT / 2)
+    time.sleep(wait_time - 4)
+    pg.press("enter")
+    #INVIO DEL MESSAGGIO - rimetto il focus sulla chat
+    pg.click(core.WIDTH / 2, core.HEIGHT / 2)
+    time.sleep(wait_time - 4)
+    pg.press("enter")
+    """ time.sleep(wait_time)
     index = 0
     length = len(message)
     while index < length:
@@ -50,7 +59,7 @@ def sendwhatmsg_instantly(
                 pg.write(letter)
                 index += 1
         index += 1
-    pg.press("enter")
+    pg.press("enter") """
     log.log_message(_time=time.localtime(), receiver=phone_no, message=message)
     if tab_close:
         core.close_tab(wait_time=close_time)
